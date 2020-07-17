7a47e942437b23cf4f7a54d292a52d0d8671255d
shaiqkar <shaiqkar@gmail.com>
2020-07-17 18:41:14 +0530

    Added Profile Sidebar

---
+++ apps/internship/yaarme/task-4-chat/static/js/app.js
@@ -0,0 +1,48 @@
+var $ = (elem) => {
+    return document.querySelector(elem)
+};
+
+(function(){
+
+    $('.p-btn').addEventListener('click', () => {
+        $('.cover').classList.add('cactive')
+        $('.profile-sidebar').classList.add('ps-active')
+
+    });
+
+    $('.sidebar-close').addEventListener('click', () => {
+        $('.profile-sidebar').classList.remove('ps-active')
+        $('.cover').classList.remove('cactive')
+    });
+
+    $('#app').addEventListener("click", () => {
+        $('.profile-sidebar').classList.remove('ps-active')
+        $('.cover').classList.remove('cactive')
+    })
+
+    var d = document,
+        tabs = $('.tabs'),
+        tab = d.querySelectorAll('.p-nav-link'),
+        contents = d.querySelectorAll('.p-nav-content');
+
+    tabs.addEventListener('click', function(e) {
+
+        if (e.target && e.target.nodeName === 'A') {
+          // change tabs
+          for (var i = 0; i < tab.length; i++) {
+            tab[i].classList.remove('p-nav-link-active');
+          }
+          e.target.classList.toggle('p-nav-link-active');
+  
+  
+          // change content
+          for (i = 0; i < contents.length; i++) {
+            contents[i].classList.remove('p-nav-t-active');
+          }
+          
+          var tabId = '#' + e.target.dataset.tabId;
+   $(tabId).classList.toggle('p-nav-t-active'); 
+        }  
+    });
+
+})();
