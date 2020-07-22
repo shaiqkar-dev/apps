475ca8d61036ae48024db383a2c3fd2ac7565bdc
shaiqkar <shaiqkar@gmail.com>
2020-07-22 20:10:42 +0530

    ./d.d

---
+++ apps/internship/yaarme/Task-6/static/js/app.js
@@ -0,0 +1,28 @@
+function toggleTextVisibility() {
+    const hiddenText = document.getElementById("hiddenText");
+    const toggleBtn =  document.getElementById("toggleText");
+    hiddenText.removeAttribute('hidden');
+    toggleBtn.setAttribute('hidden', '')
+}
+
+
+let sidenav = document.getElementById('sidenav-mobile')
+
+document.getElementById('sidebar-btn').onclick = () => { 
+    sidenav.classList.add('mobile-sidebar-active')
+}
+document.querySelector(".app").addEventListener("click", () => {
+    sidenav.classList.remove('mobile-sidebar-active')
+})
+sidenav.addEventListener('touchmove', (event) => {
+    event.preventDefault();
+}, false);
+
+let dropNav = document.getElementById('drop-nav-btn')
+dropNav.onclick = () => {
+    dropNav.classList.toggle("drop-active-btn")
+    document.getElementById('drop-nav').classList.toggle("drop-active")
+}
+
+
+
